2f5cbb19283126cd7c5cd1159a95325519e795dd
Ismael Fernandez <ishy360@gmail.com>
2019-05-11 11:32:28 -0500

    Added connection string to laptop database &  a database seed script

---
+++ Database/Tables/Customers_Seed_Script.sql
@@ -0,0 +1,29 @@
+INSERT INTO [dbo].[Customers] (
+	CustomerID,
+	FirstName,
+	LastName,
+	DateOfBirth,
+	Address
+)
+VALUES
+	(
+		N'1',
+		N'Bob',
+		N'Smith',
+		NULL,
+		N'North St.'
+	),
+	(
+		N'2',
+		N'Alex',
+		N'Jones',
+		NULL,
+		N'South St.'
+	),
+	(
+		N'3',
+		N'Jim',
+		N'Shoe',
+		NULL,
+		N'West St.'
+	);
